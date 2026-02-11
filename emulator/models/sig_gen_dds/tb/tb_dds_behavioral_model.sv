755e4471f273a83ea85c8f39b5556f9d84d6cfbe
Diego Martin <dmartin2@fnal.gov>
2026-02-11 14:21:03 -0600

    [hrl-labs-clinic-25-26/qick#47] Cleaned-up sig_gen_dds model. Moved it to emulator/models folder

--- emulator/models/sig_gen_dds/tb/tb_dds_behavioral_model.sv
+++ emulator/models/sig_gen_dds/tb/tb_dds_behavioral_model.sv
@@ -1,4 +1,4 @@
-module dds_behavioral_model_tb();
+module tb_dds_behavioral_model();
 logic           aclk;
 logic           s_axis_phase_tvalid;
 logic [71:0]    s_axis_phase_tdata;
@@ -227,11 +227,15 @@ initial begin
 	s_axis_phase_tvalid	<= 0;
 
 	#10000;
+
+	$finish();
 end
+
 always begin
 	aclk <= 0;
 	#10;
 	aclk <= 1;
 	#10;
 end
+
 endmodule
